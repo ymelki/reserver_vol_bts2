93718aa79537429a87ef835b0c8e19c132b016b7
ymelki <67371192+ymelki@users.noreply.github.com>
2024-01-22 18:13:08 +0100

    init projet

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// routeur
+// var_dump($_SERVER);
+$page=$_GET['uc'];
+
+// afficher le header
+include __DIR__.'/Vue/header.php';
+if ($page==="accueil"){
+    include __DIR__.'/Controller/AccueilController.php';
+}
+elseif ($page==="compagnie"){
+    include __DIR__.'/Controller/CompagnieController.php';
+
+}  
+elseif ($page==="aeroport"){
+    include __DIR__.'/Controller/AeroportController.php';
+}  
+elseif ($page==="inscription"){
+    echo "inscription";
+} 
+?>
+</body>
+</html>
